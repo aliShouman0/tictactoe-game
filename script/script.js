d67f509d75ad7987e3e1efb91204306046068683
Ali Shouman <alishouman010@gmail.com>
2022-09-24 01:21:03 +0300

    just testing not working yet

--- script/script.js
+++ script/script.js
@@ -20,28 +20,135 @@ images.forEach((img, i) => {
     "click",
     (img.ref = () => {
       game(img, i);
+      console.log(clicked);
     })
   );
 });
 
 // EventListener for to star game
 const game = (img, i) => {
-  if (player == 1) {
-    img.src = redImg;
-    player++;
-  } else {
-    img.src = yellowImg;
-    player--;
-  }
+  // if (player == 1) {
+  img.src = redImg;
+  player++;
+
   img.classList.remove("opacity");
   clicked[i] = player;
   checkWin();
   playerTag.textContent = player == 1 ? "Red" : "Yellow";
   scoreRedTag.textContent = redScore;
   scoreYellowTag.textContent = yellowScore;
-
   img.removeEventListener("click", img.ref);
+  bestMove();
+  // } else {
+  // img.src = yellowImg;
+  // player--;
+  // }
 };
+
+function bestMove() {
+  // AI to make its turn
+  let bestScore = -Infinity;
+  let move;
+  let i = 0,
+    j = 0;
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      // Is the spot available?
+      ai = i + j;
+      if (clicked[ai] == -1) {
+        clicked[ai] = ai;
+        let score = minimax(clicked, false);
+        clicked[ai] = -1;
+        if (score > bestScore) {
+          bestScore = score;
+          // move = { i, j };
+        }
+      }
+    }
+  }
+  images[ai].src = yellowImg;
+  player--;
+  // currentPlayer = human;
+  images[ai].classList.remove("opacity");
+  clicked[ai] = player;
+
+  playerTag.textContent = player == 1 ? "Red" : "Yellow";
+  scoreRedTag.textContent = redScore;
+  scoreYellowTag.textContent = yellowScore;
+  images[ai].removeEventListener("click", images[ai].ref);
+  checkWin();
+}
+
+let scores = {
+  X: 10,
+  O: -10,
+  tie: 0,
+};
+
+function minimax(board, isMaximizing) {
+
+  let result = checkWin1();
+  if (result !== null) {
+    return result;
+  }
+
+  if (isMaximizing) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // Is the spot available?
+        ai = i + j;
+        if (board[ai] ==-1) {
+          board[ai] = ai;
+          let score = minimax(board, false);
+          board[ai] = -1;
+          // bestScore = max(score, bestScore);
+          if (score > bestScore) {
+            board = scores;
+          }
+        }
+      }
+    }
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // Is the spot available?
+        ai = i + j;
+
+        if (board[ai] == -1) {
+          board[ai] = player - 1;
+          let score = minimax(board, false);
+          board[ai] = -1;
+          if (score < bestScore) {
+            bestScore = scores;
+          }
+        }
+      }
+    }
+    return bestScore;
+  }
+}
+
+// // EventListener for to star game
+// const game = (img, i) => {
+//   if (player == 1) {
+//     img.src = redImg;
+//     player++;
+//   } else {
+//     img.src = yellowImg;
+//     player--;
+//   }
+//   img.classList.remove("opacity");
+//   clicked[i] = player;
+//   checkWin();
+//   playerTag.textContent = player == 1 ? "Red" : "Yellow";
+//   scoreRedTag.textContent = redScore;
+//   scoreYellowTag.textContent = yellowScore;
+
+//   img.removeEventListener("click", img.ref);
+// };
 
 // reset when win or full
 const reset = () => {
@@ -130,3 +237,54 @@ const checkWin = () => {
 resetBtn.addEventListener("click", () => {
   location.reload();
 });
+
+// check if win
+const checkWin1 = () => {
+  for (let i = 0; i < 3; i++) {
+    if (
+      clicked[i] == clicked[i + 3] &&
+      clicked[i + 3] == clicked[i + 6] &&
+      clicked[i] != -1
+    ) {
+      return clicked[i];
+    }
+  }
+
+  for (let i = 0; i < 7; i += 3) {
+    if (
+      clicked[i] == clicked[i + 1] &&
+      clicked[i + 1] == clicked[i + 2] &&
+      clicked[i] != -1
+    ) {
+      return clicked[i];
+    }
+  }
+
+  if (
+    clicked[0] == clicked[4] &&
+    clicked[4] == clicked[8] &&
+    clicked[8] != -1
+  ) {
+    return clicked[0];
+  }
+
+  if (
+    clicked[2] == clicked[4] &&
+    clicked[4] == clicked[6] &&
+    clicked[6] != -1
+  ) {
+    return clicked[2];
+  }
+
+  //no win all full
+  let i = 0;
+  for (i = 0; i < clicked.length; i++) {
+    if (clicked[i] == -1) {
+      break;
+    }
+  }
+  if (i == clicked.length) {
+    setTimeout(reset, 1000);
+  }
+  return null;
+};
